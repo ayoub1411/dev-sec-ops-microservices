7f7b88f05e7610197acfd8aa4429df1a53bf4aac
Ayoub <Ayoubhass077@gmail.com>
2025-08-08 17:01:22 +0100

    update feign proxies

--- services/gateway/src/main/java/com/alibou/gateway/GatewayApplication.java
+++ services/gateway/src/main/java/com/alibou/gateway/GatewayApplication.java
@@ -1,13 +1,29 @@
 package com.alibou.gateway;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+
+	@Bean
+	CommandLineRunner commandLineRunner() {
+
+
+		return args -> {
+
+
+			System.out.println("Hello Ayoub 1");
+
+		};
 	}
 
 }
